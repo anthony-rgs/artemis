f3e42d5e3bb1a7f5d5b96bfe9953ee4595e83897
Anthony Ringressi <95974061+anthony-rgs@users.noreply.github.com>
2025-03-10 21:29:34 +0100

    feat: add timer

--- src/scraper/collection_handler.py
+++ src/scraper/collection_handler.py
@@ -1,8 +1,8 @@
+import time
 from src.config import MAX_RETRIES
 from src.utils.logger import logger 
 from src.utils.json_handler import create_json 
 from src.utils.retry import retry_function
-from src.utils.validator import check_scraped_data
 from src.utils.url_handler import check_link
 from src.utils.string_utils import format_name
 from src.scraper.driver import init_driver, close_driver
@@ -11,6 +11,9 @@ from src.scraper.scroll_manager import retry_and_check_tracks
 
 # Scrapes a collection from the given platform
 def scrape_collection(tracks_url, json_save = True ):
+  # Start script timer
+  start_scraping = time.perf_counter()
+
   check_url = check_link(tracks_url)
 
   if check_url:
@@ -35,7 +38,12 @@ def scrape_collection(tracks_url, json_save = True ):
 
     scroll_container = get_scroll_container(driver)
     
+    # Fetch tracks and start scroll timer
+    start_scrolling = time.perf_counter()
     tracks = retry_function(retry_and_check_tracks, driver, total_tracks, scroll_container, extract_tracks)
+    end_scrolling = time.perf_counter()
+    scrolling_elapsed = end_scrolling - start_scrolling
+    logger.info(f'⏰ Scrolling completed in {scrolling_elapsed:.6f} seconds\n')
 
     if not tracks:
       logger.error(f"💀 Tracks scraping failed after multiple retries... \n")
@@ -49,7 +57,12 @@ def scrape_collection(tracks_url, json_save = True ):
 
         name = retry_function(extract_name, driver)
         formatted_name = retry_function(format_name, name)
-        create_json(tracks, tracks_url, formatted_name)
+        create_json(tracks, tracks_url, formatted_name, platform, content_type)
+
+      # End global timer
+      end_scraping = time.perf_counter()
+      scraping_elapsed = end_scraping - start_scraping
+      logger.info(f'⏰ Scraping completed in {scraping_elapsed:.6f} seconds\n')
 
       logger.info(f"🎉 You rock, Artémis !\n")
 

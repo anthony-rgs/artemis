32bf22efac24f69a05bead51e2227d0b967dc175
Anthony Ringressi <95974061+anthony-rgs@users.noreply.github.com>
2025-03-10 21:30:53 +0100

    fix: add security when a track length is under 30

--- src/scraper/spotify/spotify_tracks_extractor.py
+++ src/scraper/spotify/spotify_tracks_extractor.py
@@ -2,9 +2,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from src.config import SPOTIFY_TRACKLIST_ROW_SELECTOR, SPOTIFY_MUSIC_COLUMN_SELECTOR, SPOTIFY_ALBUM_COLUMN_SELECTOR
+from src.utils.logger import logger
 
 # Extract all Spotify tracks in a page
-def spotify_extract_tracks(driver, tracks):
+def spotify_extract_tracks(driver, tracks, total_tracks):
   # Waiting for elements
   WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, SPOTIFY_TRACKLIST_ROW_SELECTOR)))
 
@@ -14,13 +15,19 @@ def spotify_extract_tracks(driver, tracks):
   # Convert tracks list to a set of tuples for fast lookup
   existing_tracks = {tuple(track) for track in tracks}
   
+  # If too many tracks are retrieved, it means recommended tracks were included, so we trim the list.
+  # This only triggers for small playlists with fewer than 25 tracks.
+  if len(page_tracks) > total_tracks:
+    page_tracks = page_tracks[0:total_tracks]  # Reset the list length to match the original playlist size.
+    logger.info(f"🔪 More tracks scraped than the actual playlist size, list trimmed")
+
   for page_track in page_tracks:
     try:
       elements = {
         "album": page_track.find_element(By.CSS_SELECTOR, SPOTIFY_ALBUM_COLUMN_SELECTOR),
         "music": page_track.find_element(By.CSS_SELECTOR, SPOTIFY_MUSIC_COLUMN_SELECTOR)
       }
-
+      
       # Links
       album_links = elements["album"].find_elements(By.TAG_NAME, 'a')
       music_links = elements["music"].find_elements(By.TAG_NAME, 'a')

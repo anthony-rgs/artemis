c9bb5ef0e92df6da7a161f07b03347d86b47896e
Anthony Ringressi <95974061+anthony-rgs@users.noreply.github.com>
2025-03-14 01:49:32 +0100

    feat: new page scroll

--- src/scraper/scroll_manager.py
+++ src/scraper/scroll_manager.py
@@ -1,23 +1,35 @@
 import time
-from src.config import SCROLL_STEP, MAX_RETRIES, RETRY_WAIT_TIME
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from src.config import CUSTOM_OVERLAY_ID, MAX_RETRIES, RETRY_WAIT_TIME, SCROLL_ITERATIONS
 from src.utils.logger import logger
 from src.utils.validator import check_scraped_data
 from src.scraper.driver import close_driver
 
 # Scrolls to the top of the page
-def scroll_to_top(driver, container):
-  driver.execute_script("arguments[0].scrollTop = 0;", container)
+def scroll_to_top(container):
+  container.send_keys(Keys.HOME)
 
 
 # Scrolls down the page
-def scroll_page(driver, container, step = SCROLL_STEP):
-  driver.execute_script("arguments[0].scrollTop += arguments[1];", container, step)
+def scroll_page(container):
+  for _ in range(SCROLL_ITERATIONS):  # 1 scroll iteration corresponds to one visible line on the page
+    container.send_keys(Keys.ARROW_DOWN)
 
 
-# Gets the current scroll position of the container
+# Get the current position of the custom div relative to the container and the global scroll offset
 def get_scroll_position(driver, container):
   try:
-    return driver.execute_script("return arguments[0].scrollTop;", container)
+    custom_div = driver.find_element(By.ID, CUSTOM_OVERLAY_ID)
+    scroll_position = driver.execute_script(
+      """
+        const rect = arguments[0].getBoundingClientRect();  // Position of custom_div
+        const containerRect = arguments[1].getBoundingClientRect();  // Position of container
+        return rect.top + window.scrollY - containerRect.top + arguments[1].scrollTop;
+      """, custom_div, container
+    )
+
+    return scroll_position
   except Exception as e:
     logger.error(f"❌ Failed to retrieve scroll position: {e}", exc_info=True)
     return None
@@ -38,14 +50,15 @@ def load_all_tracks(driver, total_tracks, container, extract_tracks):
       logger.info(f"📊 Progress: {percent_completion}%")
       progress = percent_completion
 
-    # Scroll down
-    scroll_page(driver, container)
-
+    # Simulate a scroll action
+    scroll_page(container)
+    
     # Check if scrolling is still progressing
     new_scroll = get_scroll_position(driver, container)
-    if new_scroll is None or new_scroll == last_scroll or percent_completion >= 100:
+
+    if new_scroll is None or percent_completion >= 100  or new_scroll == last_scroll:
       logger.info("✅ Scrolling completed\n")
-      break 
+      break
 
     # Extract tracks safely
     try:
@@ -77,9 +90,8 @@ def retry_and_check_tracks(driver, total_tracks, scroll_container, extract_track
         return tracks
 
       # If data is incorrect, scroll back to the top of the page
-      scroll_to_top(driver, scroll_container)
+      scroll_to_top(scroll_container)
       time.sleep(RETRY_WAIT_TIME)
-
 
     except Exception as e:
       logger.error(f"❌ Error during track extraction attempt {retry_attempts + 1}: {e}", exc_info=True)

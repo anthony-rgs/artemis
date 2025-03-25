e5d2727920039d88ab5b1bf3be847abf5dd5e10c
Anthony Ringressi <95974061+anthony-rgs@users.noreply.github.com>
2025-03-25 15:34:17 +0100

    feat: add dynamic scraper script in a function

--- dynamic_scraper.py
+++ dynamic_scraper.py
@@ -1,11 +1,16 @@
 from src.utils.logger import logger 
 from src.scraper.collection_handler import scrape_collection
 
-logger.info("🎫 Dynamic script running...\n")
+# Dynamic scraper script
+def dynamic_scaper():
+  logger.info("🎫 Dynamic script running...\n")
 
-logger.info("🌐 Enter the URL: ")
-url = input()
+  logger.info("🌐 Enter the URL: ")
+  url = input()
 
-print("")
+  print("")
 
-scrape_collection(url)
+  scrape_collection(url)
+
+
+dynamic_scaper()
